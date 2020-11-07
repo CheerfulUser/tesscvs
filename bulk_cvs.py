fc0a8187560e0e0a8999fa7818c69655e39b7e73
Ryan Ridden-Harper <rridden@ryanr.local.stsci.edu>
2020-11-07 18:50:15 -0500

    try wrap

--- bulk_cvs.py
+++ bulk_cvs.py
@@ -33,93 +33,95 @@ for j in range(len(cvs)-7):
 	c = SkyCoord(ra=float(ra)*u.degree, dec=float(dec) *u.degree, frame='icrs')
 
 	tess = lk.search_tesscut(c,sector=None)
+	try:
+		if len(tess) > 0:
+			lcs = []
+			zps = []
+			err = []
+			sectors = []
+			trends1 = []
+			trends2 = []
 
-	if len(tess) > 0:
-		lcs = []
-		zps = []
-		err = []
-		sectors = []
-		trends1 = []
-		trends2 = []
+			if len(tess) > 1:
+				tpfs = []
+				for t in tess:
+					tpf = t.download(cutout_size=40)
+					#aper_b18 = np.zeros(tpf.shape[1:], dtype=bool)
+					#aper_b18[44:48, 44:47] = True
+					res = tr.Quick_reduce(tpf)#,aper=aper_b18)
+					lcs += [res['lc']]
+					err += [res['err']]
+					zps += [res['zp']]
+					sectors += [tpf.sector]
+					try:
+						print('trend error in {} sector {}'.format(cv['Names'],tpf.sector))
+						trends1 += [tr.Remove_stellar_variability(lcs[-1],err[-1],variable=True)]
+						trends2 +=  [tr.Remove_stellar_variability(lcs[-1],err[-1],variable=False)]
+					except:
+						filler = np.nan * np.ones(len(err[-1]))
+						trends1 += [filler]
+						trends2 == [filler]
+					
 
-		if len(tess) > 1:
-			tpfs = []
-			for t in tess:
-				tpf = t.download(cutout_size=40)
-				#aper_b18 = np.zeros(tpf.shape[1:], dtype=bool)
-				#aper_b18[44:48, 44:47] = True
-				res = tr.Quick_reduce(tpf)#,aper=aper_b18)
-				lcs += [res['lc']]
-				err += [res['err']]
-				zps += [res['zp']]
-				sectors += [tpf.sector]
-				try:
-					print('trend error in {} sector {}'.format(cv['Names'],tpf.sector))
-					trends1 += [tr.Remove_stellar_variability(lcs[-1],err[-1],variable=True)]
-					trends2 +=  [tr.Remove_stellar_variability(lcs[-1],err[-1],variable=False)]
-				except:
-					filler = np.nan * np.ones(len(err[-1]))
-					trends1 += [filler]
-					trends2 == [filler]
+				name = cv['Names']
+				plt.figure(figsize=(6.5,8))
+				plt.subplot(311)
+				plt.title(name)
+				for i in range(len(lcs)):
+					plt.plot(lcs[i][0],lcs[i][1],label='S ' + str(sectors[i]))
+				plt.legend()
+				plt.ylabel('Counts')
+
+				plt.subplot(312)
+				plt.title('trend method 1')
+				for i in range(len(lcs)):
+					plt.fill_between(lcs[i][0],lcs[i][1]-trends1[i]-err[i],lcs[i][1]-trends1[i]+err[i],alpha=.5)
+					plt.plot(lcs[i][0],lcs[i][1]-trends1[i])
+				plt.ylabel('Counts')
 				
+				plt.subplot(313)
+				plt.title('trend method 2')
+				for i in range(len(lcs)):
+					plt.fill_between(lcs[i][0],lcs[i][1]-trends2[i]-err[i],lcs[i][1]-trends2[i]+err[i],alpha=.5)
+					plt.plot(lcs[i][0],lcs[i][1]-trends2[i])
+				plt.ylabel('Counts')
+				plt.xlabel('MJD')
+				plt.tight_layout()
 
-			name = cv['Names']
-			plt.figure(figsize=(6.5,8))
-			plt.subplot(311)
-			plt.title(name)
-			for i in range(len(lcs)):
-				plt.plot(lcs[i][0],lcs[i][1],label='S ' + str(sectors[i]))
-			plt.legend()
-			plt.ylabel('Counts')
+				savename = name.replace('/',' ').replace(' ','_')
+				plt.savefig('./figs/{}.pdf'.format(savename))
 
-			plt.subplot(312)
-			plt.title('trend method 1')
-			for i in range(len(lcs)):
-				plt.fill_between(lcs[i][0],lcs[i][1]-trends1[i]-err[i],lcs[i][1]-trends1[i]+err[i],alpha=.5)
-				plt.plot(lcs[i][0],lcs[i][1]-trends1[i])
-			plt.ylabel('Counts')
-			
-			plt.subplot(313)
-			plt.title('trend method 2')
-			for i in range(len(lcs)):
-				plt.fill_between(lcs[i][0],lcs[i][1]-trends2[i]-err[i],lcs[i][1]-trends2[i]+err[i],alpha=.5)
-				plt.plot(lcs[i][0],lcs[i][1]-trends2[i])
-			plt.ylabel('Counts')
-			plt.xlabel('MJD')
-			plt.tight_layout()
+				# save to cvs
+				mjd = lcs[0][0].copy()
+				flux = lcs[0][1].copy()
+				e = err[0].copy()
+				t1 = trends1[0].copy()
+				t2 = trends2[0].copy()
+				z = np.ones(len(lcs[0][0])) * zps[0][0]
+				s = np.ones(len(lcs[0][0])) * sectors[0]
+				for i in range(len(lcs)-1):
+					i += 1
+					mjd = np.append(mjd,lcs[i][0])
+					flux = np.append(flux,lcs[i][1])
+					e = np.append(e,err[i])
+					t1 = np.append(t1,trends1[i])
+					t2 = np.append(t2,trends2[i])
 
-			savename = name.replace('/',' ').replace(' ','_')
-			plt.savefig('./figs/{}.pdf'.format(savename))
+					zz = np.ones(len(lcs[i][0])) * zps[i][0]
+					ss = np.ones(len(lcs[i][0])) * sectors[i]
+					z = np.append(z,zz)
+					s = np.append(s,ss)
+				df = pd.DataFrame(columns=['mjd','flux','err','trend1','trend2','zp','sector'])
+				df['mjd'] = mjd
+				df['flux'] = flux
+				df['err'] = e
+				df['trend1'] = t1
+				df['trend2'] = t2
+				df['zp'] = z
+				df['sector'] = s
 
-			# save to cvs
-			mjd = lcs[0][0].copy()
-			flux = lcs[0][1].copy()
-			e = err[0].copy()
-			t1 = trends1[0].copy()
-			t2 = trends2[0].copy()
-			z = np.ones(len(lcs[0][0])) * zps[0][0]
-			s = np.ones(len(lcs[0][0])) * sectors[0]
-			for i in range(len(lcs)-1):
-				i += 1
-				mjd = np.append(mjd,lcs[i][0])
-				flux = np.append(flux,lcs[i][1])
-				e = np.append(e,err[i])
-				t1 = np.append(t1,trends1[i])
-				t2 = np.append(t2,trends2[i])
+				df.to_csv('./lcs/{}.csv'.format(savename),index=False)
 
-				zz = np.ones(len(lcs[i][0])) * zps[i][0]
-				ss = np.ones(len(lcs[i][0])) * sectors[i]
-				z = np.append(z,zz)
-				s = np.append(s,ss)
-			df = pd.DataFrame(columns=['mjd','flux','err','trend1','trend2','zp','sector'])
-			df['mjd'] = mjd
-			df['flux'] = flux
-			df['err'] = e
-			df['trend1'] = t1
-			df['trend2'] = t2
-			df['zp'] = z
-			df['sector'] = s
-
-			df.to_csv('./lcs/{}.csv'.format(savename),index=False)
-
-			print('finished {}'.format(name))
+				print('finished {}'.format(name))
+	except:
+		pass
